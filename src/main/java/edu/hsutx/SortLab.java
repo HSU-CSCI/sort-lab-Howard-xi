a837c1980a0440e8ed28895975567be318e09569
Howard-xi <huang.shihao@mcm.edu>
2025-11-14 00:20:31 -0600

    unfinished sortLab

--- src/main/java/edu/hsutx/SortLab.java
+++ src/main/java/edu/hsutx/SortLab.java
@@ -65,37 +65,94 @@ public class SortLab {
     // implementation of Radix Sort
     public static void radixSort(int[] array) {
         // TODO - Implement Radix Sort
+        int max = findMax(array);
+
+        int radix = 1;
+        int n = array.length;
+        int[] output = new int[n];
+
+        while (max/radix > 0) {
+            int[] count = new int[10];
+            for (int i = 0; i < n; i++) {
+                int num = (array[i] / radix) % 10;
+                count[num]++;
+            }
+            for (int i = 1; i < 10; i++) {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = n - 1; i >= 0; i--) {
+                int num = (array[i] / radix) % 10;
+                output[count[num] - 1] = array[i];
+                count[num]--;
+            }
+            System.arraycopy(output, 0, array, 0, n);
+            radix *= 10;
+        }
     }
 
     // implementation of your choice of merge, pivot, heap or other O(nlogn) comparisonSort
     public static void comparisonSort(int[] array) {
         // TODO - Replace this selection sort implementation with a comparision sort of your choice.
-        int maxIndex = 0;
-        for (int i = 0; i < array.length-1; i++) {
-            maxIndex = i;
-            for (int j = i+1; j < array.length; j++)
-                if (array[j] > array[maxIndex]) maxIndex = j;
+        mergeSort(array, 0, array.length -1);
+    }
 
-            // swap using bitwise operations
-            array[i] = array[i] ^ array[maxIndex];
-            array[maxIndex] = array[i] ^ array[maxIndex];
-            array[i] = array[i] ^ array[maxIndex];
+    private static void mergeSort(int[] arr, int l, int r) {
+        if (l >= r) return;
+
+        int mid = (l + r) / 2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid+1, r);
+        merge(arr, l, mid, r);
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        int[] temp = new int[r - l + 1];
+        int i = l, j = m+1, k = 0;
+
+        while (i <= m && j <= r) {
+            if (arr[i] <= arr[j]) temp[k++] = arr[i++];
+            else temp[k++] = arr[j++];
         }
+
+        while (i <= m) temp[k++] = arr[i++];
+        while (j <= r) temp[k++] = arr[j++];
+
+        System.arraycopy(temp, 0, arr, l, temp.length);
     }
 
     // implementation of your choice of merge, pivot, heap or other O(nlogn) comparisonSort
     public static void secondComparisonSort(int[] array) {
         // TODO - Replace this selection sort implementation with a comparision sort of your choice.
-        int maxIndex = 0;
-        for (int i = 0; i < array.length-1; i++) {
-            maxIndex = i;
-            for (int j = i+1; j < array.length; j++)
-                if (array[j] > array[maxIndex]) maxIndex = j;
+       heapSort(array);
+    }
 
-            // swap using bitwise operations
-            array[i] = array[i] ^ array[maxIndex];
-            array[maxIndex] = array[i] ^ array[maxIndex];
-            array[i] = array[i] ^ array[maxIndex];
+    private static void heapSort(int[] array) {
+        int n = array.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(array, n, i);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            heapify(array, i, 0);
+        }
+    }
+
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) largest = left;
+        if (right < n && arr[right] > arr[largest]) largest = right;
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, n, largest);
         }
     }
 
@@ -125,7 +182,7 @@ public class SortLab {
             timer.start();
             comparisonSort(sortArray);
             timer.stop();
-            System.out.println("    Comparison Sort: "+timer);  //TODO - Change name to your actual sort
+            System.out.println("    Merge Sort: "+timer);  //TODO - Change name to your actual sort
 
             // Second Comparison Sort
             sortArray=array.clone();
@@ -133,7 +190,7 @@ public class SortLab {
             timer.start();
             secondComparisonSort(sortArray);
             timer.stop();
-            System.out.println("    Comparison Sort: "+timer);  //TODO - Change name to your actual sort
+            System.out.println("    Heap Sort: "+timer);  //TODO - Change name to your actual sort
 
             // Counting Sort
             sortArray=array.clone();
